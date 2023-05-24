398e914e1a3ee7dba4c0dd45e5debf9f93bbc29e
ChrisLee1659 <119467134+ChrisLee1659@users.noreply.github.com>
2023-05-23 19:48:13 -0500

    added login script for the login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login(props) {
-    return (
-        <div>
-            <h2>this it the login area</h2>
-        </div>
-    );
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+        props.history.push('/');
+        } else {
+         alert('Invalid email or password');
+        }
+      });
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
