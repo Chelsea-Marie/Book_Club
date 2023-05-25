9822ad09cb9cd9a23565567442e066b059f35aa4
chel.c <chelsea.marie.pederson@gmail.com>
2023-05-25 18:24:56 -0500

    path thing

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { ApolloServer } = require('apollo-server-express');
 const db = require('./config/connection');
+const path = require('path')
 
 //queries and mutations to db
 const { typeDefs, resolvers } = require('./schemas');
